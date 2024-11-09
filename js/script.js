d1d52ec953e9383296b88da179b156e3b8c2ffcc
DaisyChen <xhsuan1215@gmail.com>
2024-11-10 02:16:39 +0800

    第一版

---
+++ js/script.js
@@ -0,0 +1,121 @@
+// console.log("Hello world!");
+
+//////////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+//////////////////////////////////////////////////////////////
+// mobile navigation
+const searchBtn = document.querySelector(".search-btn");
+const closeBtn = document.querySelector(".close-btn");
+const searchBox = document.querySelector(".search-box");
+const mainNavList = document.querySelector(".main-nav-list");
+
+const navBtnEl = document.querySelector(".mobile-nav-btn");
+const headerEl = document.querySelector("header");
+
+searchBtn.addEventListener("click", function () {
+  searchBox.classList.add("active");
+  searchBtn.classList.add("active");
+  closeBtn.classList.add("active");
+  navBtnEl.classList.add("close");
+  mainNavList.classList.remove("open");
+});
+
+closeBtn.addEventListener("click", function () {
+  searchBox.classList.remove("active");
+  searchBtn.classList.remove("active");
+  closeBtn.classList.remove("active");
+  navBtnEl.classList.remove("close");
+});
+
+navBtnEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+  searchBox.classList.remove("active");
+  searchBtn.classList.remove("active");
+  closeBtn.classList.remove("active");
+});
+
+//////////////////////////////////////////////////////////////
+//smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    //     e.preventDefault();
+    //     const href = link.getAttribute("href");
+    //     // console.log(href);
+
+    //     // Scroll back to top:
+    //     if (href === "#");
+    //     window.scrollTo({
+    //       top: 0,
+    //       behavior: "smooth",
+    //     });
+
+    //     if (href !== "#" && href.startsWith("#")) {
+    //       const sectionEL = document.querySelector(href);
+    //       sectionEL.scrollIntoView({ behavior: "smooth" });
+    //     }
+
+    // Close mobile navigation:
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+//////////////////////////////////////////////////////////////
+//Sticky navigation
+// const sectionHeroEl = document.querySelector(".section-hero");
+
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
+
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
+
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     // In the viewport
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(sectionHeroEl);
+
+// window.addEventListener("scroll", function () {
+//   headerEl.classList.toggle("sticky", window.scrollY > 0);
+// });
+
+//////////////////////////////////////////////////////////////
+//Login
+const loginWrap = document.querySelector(".login-wrap");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const loginPopup = document.querySelector(".main-nav-btn");
+const loginClose = document.querySelector(".login-close-icon");
+
+registerLink.addEventListener("click", () => {
+  loginWrap.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  loginWrap.classList.remove("active");
+});
+
+loginPopup.addEventListener("click", () => {
+  loginWrap.classList.add("active-popup");
+});
+
+loginClose.addEventListener("click", () => {
+  loginWrap.classList.remove("active-popup");
+});
